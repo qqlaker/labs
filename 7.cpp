2edfaccd78771ddebb2e016405fbf6913fe5db9e
qqlaker <yayaya.tytytyty@mail.ru>
2021-10-08 11:09:08 +0300

    7

---
+++ 7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+	switch (a)
+	{
+	case 1: cout << "brown"; break;
+	case 2: cout << "gray"; break;
+	case 3: cout << "yellow"; break;
+	case 4: cout << "dark green"; break;
+	case 5: cout << "light red"; break;
+	case 6: cout << "black"; break;
+	case 7: cout << "purple"; break;
+	case 8: cout << "orange"; break;
+	case 9: cout << "cyan"; break;
+	case 10: cout << "lime"; break;
+	default: cout << "";
+	}
+}
